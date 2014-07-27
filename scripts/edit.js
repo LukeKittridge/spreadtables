b2f3e7f62ddc0e3a9d0b753841e8c4ac8311e154
Luke Kittridge <luke.kittridge@gmail.com>
2014-07-27 17:20:21 +0100

    Added basic onblur event.

--- scripts/edit.js
+++ scripts/edit.js
@@ -2,14 +2,20 @@
  * Created by Luke Kittridge on 27/07/2014.
  */
 
-function setUpGrid (){
+function setUpGrid() {
     var table1 = document.getElementById('table1');
-    for(var i =0; i < 10; i++){
-       for(var j =0; j < 10; j++){
-           var cell = document.createElement('div');
-           cell.className = "cell";
-           cell.contentEditable = 'true';
-           table1.appendChild(cell);
-       }
+    for (var i = 0; i < 10; i++) {
+        var cell = document.createElement('div');
+        cell.className = "cell";
+        cell.id = i;
+        cell.contentEditable = 'true';
+        cell.onblur =  function(cell){update(cell.target)}
+        table1.appendChild(cell);
     }
+};
+
+function update(cell){
+    var target = document.getElementById('4');
+    //TODO replace eval with some sort of language parser
+    target.innerHTML =  eval(cell.innerHTML);
 };
