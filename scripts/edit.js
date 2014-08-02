c0d1d76262d357cfe879a9f7a2f1e91daca124b3
Luke Kittridge <luke.kittridge@gmail.com>
2014-08-02 17:21:41 +0100

    The spreadsheet is now generated using the specified number of rows and columns. Updated CSS to resemble a spreadsheet.

--- scripts/edit.js
+++ scripts/edit.js
@@ -2,20 +2,31 @@
  * Created by Luke Kittridge on 27/07/2014.
  */
 
-function setUpGrid() {
-    var table1 = document.getElementById('table1');
-    for (var i = 0; i < 10; i++) {
-        var cell = document.createElement('div');
-        cell.className = "cell";
-        cell.id = i;
-        cell.contentEditable = 'true';
-        cell.onblur =  function(cell){update(cell.target)}
-        table1.appendChild(cell);
+function createTable(rows, columns) {
+    var left = 0;
+    var top = 0;
+    var cellId = 1;
+    var table = document.getElementById('table1');
+    for (var i = 0; i < rows; i++) {
+        for(var j = 0; j < columns; j++){
+            var cell = document.createElement('div');
+            cell.className = "cell";
+            cell.id = cellId;
+            cellId++;
+            cell.contentEditable = 'true';
+            cell.style.left = left + 'px';
+            cell.style.top = top + 'px';
+            cell.onblur =  function(cell){update(cell.target)}
+            table.appendChild(cell);
+            left += 100;
+        }
+        left = 0;
+        top += 22;
     }
 };
 
 function update(cell){
-    var target = document.getElementById('4');
+    var target = document.getElementById('1');
     //TODO replace eval with some sort of language parser
     target.innerHTML =  eval(cell.innerHTML);
 };
