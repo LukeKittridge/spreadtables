ddd457cb36bd26607e1b91afa1923339054c2468
Luke Kittridge <luke.kittridge@gmail.com>
2014-09-03 16:53:50 +0100

    Added extra tests for referencing cells.

--- scripts/test/cellSpec.js
+++ scripts/test/cellSpec.js
@@ -12,15 +12,23 @@ describe("Cell Test Suite", function() {
        expect(cell1.value).toEqual(45);
 
        var cell2 = getGlobalCell("#test.C1");
+       var cell3 = getGlobalCell("#test.CB14");
+
        cell2.evaluateNewFormula("#test.B1 + 5");
+       cell3.evaluateNewFormula("#test.C1 + #test.B1");
        expect(cell2.value).toEqual(50);
+       expect(cell3.value).toEqual(95);
 
        cell1.evaluateNewFormula("100");
        expect(cell2.value).toEqual(105);
+        expect(cell3.value).toEqual(205);
+
 
        expect(cell1.referencedBy[cell2.id]).toBeDefined();
         cell2.evaluateNewFormula("23");
        expect(cell1.referencedBy[cell2.id]).toBeUndefined();
+
+       expect(cell3.value).toEqual(123);
 
    });
 });
