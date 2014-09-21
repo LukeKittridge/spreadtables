799004cedf4ec09471572a10206d34746d4f42e9
Luke Kittridge <luke.kittridge@gmail.com>
2014-09-21 12:58:37 +0100

    Starting linking the backend to the front end.

--- scripts/edit.js
+++ scripts/edit.js
@@ -2,28 +2,35 @@
  * Created by Luke Kittridge on 27/07/2014.
  */
 
-function createTable(rows, columns) {
+function createTable(name, rows, columns) {
+    var table = new Table(name, rows, columns);
+    Table.tables[table.name] = table;
+    drawTable(table);
+};
+
+function drawTable(table){
+    var docTable = document.createElement('div');
+    docTable.id = table.name;
+    //ToDo Improve the drawing process
     var left = 0;
     var top = 0;
-    var cellId = 1;
-    var table = document.getElementById('table1');
-    for (var i = 0; i < rows; i++) {
-        for(var j = 0; j < columns; j++){
-            var cell = document.createElement('div');
-            cell.className = "cell";
-            cell.id = cellId;
-            cellId++;
-            cell.contentEditable = 'true';
-            cell.style.left = left + 'px';
-            cell.style.top = top + 'px';
-            cell.onblur =  function(cell){update(cell.target)}
-            table.appendChild(cell);
-            left += 100;
+    for(var i =0; i < table.cells.length; i++){
+        for(var j =0; j< table.cells[0].length; j++){
+            var docCell = document.createElement('div');
+            docCell.className = "cell";
+            docCell.id = table.cells[i][j].id;
+            docCell.contentEditable = "true";
+            docCell.style.left = left + "px";
+            docCell.style.top = top + "px";
+            docTable.appendChild(docCell);
+            left +=100;
         }
-        left = 0;
-        top += 22;
+        left =0;
+        top+=22;
     }
-};
+
+    document.body.appendChild(docTable);
+}
 
 function update(cell){
     var target = document.getElementById('1');
